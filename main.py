b974d56d6f745867eaeca83cb31030a31637833f
Federico Foti <fotifederico@gmail.com>
2025-08-21 19:18:21 -0300

    Add files via upload

---
+++ main.py
@@ -0,0 +1,9 @@
+import flet as ft
+
+
+def main(page: ft.Page):
+    page.title = "Mi primer app en Flet Cloud"
+    page.add(ft.Text("¡Hola DJ Fede desde Flet Cloud! 🚀", size=30, color="blue"))
+
+
+ft.app(target=main)
