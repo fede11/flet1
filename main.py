d1ba1469eb5120b36fca1de683ef01d73eaa1fa5
Federico Foti <fotifederico@gmail.com>
2025-08-21 19:39:32 -0300

    Update main.py

--- main.py
+++ main.py
@@ -5,5 +5,5 @@ def main(page: ft.Page):
     page.title = "Mi primer app en Flet Cloud"
     page.add(ft.Text("¡Hola DJ Fede desde Flet Cloud! 🚀", size=30, color="blue"))
 
+ft.app(target=main, view=ft.AppView.WEB_BROWSER)
 
-ft.app(target=main)
